6b74ce8decbf9fb1c237c5c8c8b2064a44355f54
Teddy <134784483+Tedmyles@users.noreply.github.com>
2025-02-26 22:20:43 +0300

    Update index.js

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,21 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import { fileURLToPath } from "url";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
-
-// Fix __dirname for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,22 +25,18 @@ app.use(
   })
 );
 
-// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve frontend files in production
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../front-end/dist");
-  app.use(express.static(frontendPath));
+  app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../fron-tend", "dist", "index.html"));
   });
 }
 
-// Start Server
 server.listen(PORT, () => {
-  console.log("Server is running on PORT:", PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
